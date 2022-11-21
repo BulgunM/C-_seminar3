0ba95011ae02d59603d705860d9c7d8d6adeb5b7
Bulgun <bulgunya@mail.ru>
2022-11-22 00:55:18 +0300

    add seminar3

---
+++ Task20/Program.cs
@@ -0,0 +1,24 @@
+﻿// принимает на вход координаты двух точек и
+// находит расстояние между ними в 2D пространстве.
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+Console.WriteLine("Введите координаты первой точки");
+Console.Write("X: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки");
+Console.Write("X: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance(int xb, int yb, int xc, int yc)
+{
+    return Math.Sqrt((xc - xb) * (xc - xb) + (yc - yb) * (yc - yb));
+
+}
+double res = Math.Round(Distance(x1, y1, x2, y2), 2, MidpointRounding.ToZero);
+Console.WriteLine(res);
---
+++ Task22/Program.cs
@@ -0,0 +1,20 @@
+﻿// принимает на вход число (N)
+// выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Squares(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        int square = count * count;
+        Console.WriteLine($"{count,4} {square,4}");
+        count++;
+    }
+}
+
+if (number > 0)
+    Squares(number);
+else Console.WriteLine("Число не является натуральным");
