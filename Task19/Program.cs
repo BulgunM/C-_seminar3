1c0c60acc63f8bb03b032dc0c64a0a9cd0570430
Bulgun <bulgunya@mail.ru>
2022-11-22 23:26:17 +0300

    add hometask3

---
+++ Task19/Program.cs
@@ -0,0 +1,24 @@
+﻿// на вход пятизначное число и 
+// проверяет, является ли оно палиндромом.
+// Выполнить с помощью числовых операций (целочисленное деление, остаток от деления).
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+bool Palindrome(int num)
+{
+    if ((num / 10000 == num % 10) && (num / 1000 % 10 == num % 100 / 10)) return true;
+    return false;
+}
+
+if (number > 9999 && number < 100000)
+{
+    bool result = Palindrome(number);
+    Console.WriteLine(result);
+}
+else Console.WriteLine("Введенное число не является пятизначным");
---
+++ Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿//на вход число (N)
+//выдаёт таблицу кубов чисел от 1 до N.
+
+// 5 ->
+// 1 | 1
+// 2 | 8
+// 3 | 27
+// 4 | 64
+// 5 | 125
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        double cube = Math.Pow(count, 3);
+        Console.WriteLine($"{count,4} | {cube,4}");
+        count++;
+    }
+}
+
+if (number > 0)
+    Cube(number);
+else Console.WriteLine("Число не является натуральным");
